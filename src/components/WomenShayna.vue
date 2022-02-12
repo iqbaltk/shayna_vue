5d98a373765197f5752bad0046caeedd7f519601
iqbaltk <iqbaltri156@gmail.com>
2022-02-12 15:05:48 +0700

    implementasi API

--- src/components/WomenShayna.vue
+++ src/components/WomenShayna.vue
@@ -1,76 +1,120 @@
 <template>
-    <div>
+  <div>
     <!-- Women Banner Section Begin -->
     <section class="women-banner spad">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12 mt-5" v-if="products.length > 0">
-                    <carousel class="product-slider" :items="3" :nav="false" :autoplay="true" :dots="false">
-                        
-                        <div class="product-item" v-for="itemProduct in products" :key="itemProduct.id">
-                            <div class="pi-pic">
-                                <img :src="itemProduct.galleries[0].photo" alt="Foto" />
-                                <ul>
-                                    <li class="w-icon active">
-                                      <router-link to="/product"><i class="icon_bag_alt"></i></router-link>
-                                    </li>
-                                    <li class="quick-view"><router-link :to="'/product/'+itemProduct.id">+ Quick View</router-link></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">{{ itemProduct.type }}</div>
-                                <router-link to="/product">
-                                    <h5>{{ itemProduct.name }}</h5>
-                                </router-link>
-                                <div class="product-price">
-                                    {{ itemProduct.price }}
-                                </div>
-                            </div>
-                        </div>
-
-                    </carousel>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-12 mt-5" v-if="products.length > 0">
+            <carousel
+              class="product-slider"
+              :items="3"
+              :nav="false"
+              :autoplay="true"
+              :dots="false"
+            >
+              <div
+                class="product-item"
+                v-for="itemProduct in products"
+                :key="itemProduct.id"
+              >
+                <div class="pi-pic">
+                  <img :src="itemProduct.galleries[0].photo" alt="Foto" />
+                  <ul>
+                    <li
+                      class="w-icon active"
+                      @click="
+                        saveKeranjang(
+                          itemProduct.id,
+                          itemProduct.name,
+                          itemProduct.price,
+                          itemProduct.galleries[0].photo,
+                        )
+                      "
+                    >
+                      <a href="#">
+                        <i class="icon_bag_alt"></i>
+                      </a>
+                    </li>
+                    <li class="quick-view">
+                      <router-link :to="'/product/' + itemProduct.id">
+                        + Quick View
+                      </router-link>
+                    </li>
+                  </ul>
                 </div>
+                <div class="pi-text">
+                  <div class="catagory-name">{{ itemProduct.type }}</div>
+                  <router-link to="/product">
+                    <h5>{{ itemProduct.name }}</h5>
+                  </router-link>
+                  <div class="product-price">
+                    {{ itemProduct.price }}
+                  </div>
+                </div>
+              </div>
+            </carousel>
+          </div>
 
-            <div class="col-lg-12" v-else>
-                <p>
-                    Produk Tidak Tersedia
-                </p>
-            </div>
-
-            </div>
+          <div class="col-lg-12" v-else>
+            <p>
+              Produk Tidak Tersedia
+            </p>
+          </div>
         </div>
+      </div>
     </section>
     <!-- Women Banner Section End -->
-    </div>
+  </div>
 </template>
 
 <script>
-
 import carousel from 'vue-owl-carousel'
 import axios from 'axios'
 
-    export default {
-        name: 'WomenShayna',
-        components:{
-            carousel
-        },
-        data(){
-            return{
-                products: []
-            };
-        },
-        mounted(){
-        axios
-            .get("https://shayna-backend.dev/api/products")
-            .then(res => (this.products = res.data.data.data))
-            .catch(err => console.log(err));
-        }
-    };
+export default {
+  name: 'WomenShayna',
+  components: {
+    carousel,
+  },
+  data() {
+    return {
+      products: [],
+      keranjangUser: [],
+    }
+  },
+  methods: {
+    saveKeranjang(idProduct, nameProduct, priceProduct, photoProduct) {
+      var productStored = {
+        id: idProduct,
+        name: nameProduct,
+        price: priceProduct,
+        photo: photoProduct,
+      }
+      this.keranjangUser.push(productStored)
+      const parsed = JSON.stringify(this.keranjangUser)
+      localStorage.setItem('keranjangUser', parsed)
+
+      window.location.reload()
+    },
+  },
+  mounted() {
+    axios
+      .get('https://shayna-backend.dev/api/products')
+      .then((res) => (this.products = res.data.data.data))
+      .catch((err) => console.log(err))
+    if (localStorage.getItem('keranjangUser')) {
+      try {
+        this.keranjangUser = JSON.parse(localStorage.getItem('keranjangUser'))
+      } catch (e) {
+        localStorage.removeItem('keranjangUser')
+      }
+    }
+  },
+}
 </script>
 
 <style scoped>
-.product-item{
-    margin-right: 25px;
+.product-item {
+  margin-right: 25px;
 }
-
 </style>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -59,14 +59,12 @@
                     <h3>{{ productDetails.name }}</h3>
                   </div>
                   <div class="pd-desc">
-                    <p>
-                      {{ productDetails.description }}
-                    </p>
+                    <p v-html="productDetails.description"></p>
                     <h4>Rp. {{ productDetails.price }}</h4>
                   </div>
                   <div class="quantity">
                     <router-link to="/cart">
-                      <a href="#" class="primary-btn pd-cart">Add To Cart</a>
+                      <a href="#"  @click="saveKeranjang(productDetails.id, productDetails.name, productDetails.price, productDetails.galleries[0].photo)"  class="primary-btn pd-cart">Add To Cart</a>
                     </router-link>
                   </div>
                 </div>
@@ -104,13 +102,8 @@ export default {
   data() {
     return {
       gambar_default: '',
-      thumbs: [
-        'img/mickey1.jpg',
-        'img/mickey2.jpg',
-        'img/mickey3.jpg',
-        'img/mickey4.jpg',
-      ],
       productDetails: [],
+      keranjangUser: []
     }
   },
   methods: {
@@ -123,8 +116,27 @@ export default {
       //ganti isi gambar_default dengan data API
       this.gambar_default = data.galleries[0].photo
     },
+    saveKeranjang(idProduct, nameProduct, priceProduct, photoProduct){
+
+      var productStored = {
+        "id": idProduct,
+        "name": nameProduct,
+        "price": priceProduct,
+        "photo": photoProduct
+      }
+      this.keranjangUser.push(productStored);
+      const parsed = JSON.stringify(this.keranjangUser);
+      localStorage.setItem('keranjangUser', parsed);
+    }
   },
   mounted() {
+    if (localStorage.getItem('keranjangUser')) {
+      try {
+        this.keranjangUser = JSON.parse(localStorage.getItem('keranjangUser'));
+      } catch(e) {
+        localStorage.removeItem('keranjangUser');
+      }
+    }
     axios
       .get('https://shayna-backend.dev/api/products', {
         params: {
